9fe9f4fc4e7ecf026728ae9ed94b791d630d1078
ashow83 <adsf83@gmail.com>
2022-11-03 21:05:40 -0300

    Create 011_arraysListas.js
    Bootcamp DIO javascript

---
+++ Dio/011_arraysListas.js
@@ -0,0 +1,23 @@
+// lembrando 
+
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(6.75);
+notas.push(5);
+notas.push(8);
+notas.push(10);
+
+let somaNotas = 0;
+
+for (let index = 0; index < notas.length; index++) {
+    somaNotas = somaNotas + notas[index];
+    
+}
+
+
+
+console.log('Você fez ' + notas.length + ' prova(s) e sua média foi '+ (somaNotas/notas.length).toFixed(2));
