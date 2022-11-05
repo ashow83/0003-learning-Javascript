22c931bbcdefc4e378b9cf4700e61187c295561b
ashow83 <adsf83@gmail.com>
2022-11-05 16:40:51 -0300

    importação
    bootcamp DIO javascript

--- Dio/014_funcoes-aux.js
+++ Dio/014_funcoes-aux.js
@@ -1,13 +1,20 @@
 
+const entradas = [5, 50, 10, 98, 23, 500, 150];
+let i = 0;
 
 function gets() {
-    return 'Adriano';
+    const valor = entradas[i];
+    i++;
+    return valor;
 }
 
+function teste() {
+    return entradas.length;
+}
 
 function print(texto) {
     console.log(texto);
 
 }
 
-module.exports = { gets, print };
+module.exports = { gets, teste, print };
--- Dio/014_importacao.js
+++ Dio/014_importacao.js
@@ -1,10 +1,52 @@
 
 
-const {gets, print}  = require('./014_funcoes-aux');
+const { gets, teste, print } = require('./014_funcoes-aux');
+
+/* 
+
+Uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 -100
+Faça um programa que receba os 5 numeros sorteados para os alunos e mostre o maior numero sorteado. 
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+Saída:
+98
 
 
-print(gets());
+*/
 
+const numSorteio = [];
+let maiorValor = 0;
+
+for (let i = 0; i < teste(); i++) {
+    const valor = gets();
+    numSorteio.push(valor);
+    if (valor > maiorValor){
+        maiorValor = valor;
+    }
+
+}
+
+
+/*
+for (let i = 0; i < numSorteio.length; i++) {
+    const valor = numSorteio[i];
+    if (valor > maiorValor) {
+        maiorValor = valor;
+    }
+
+}
+*/
+
+console.log(numSorteio);
+console.log(maiorValor);
+
+//print(teste());
 
 //console.log(funcoes.gets());
 
