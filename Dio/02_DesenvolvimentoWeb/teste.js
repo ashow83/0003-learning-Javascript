0e00d697fedc234699400459d8b952395408a627
ashow83 <adsf83@gmail.com>
2022-11-07 21:45:39 -0300

    atualização html css
    bootcamp DIO Desenvolvimento web

---
+++ Dio/02_DesenvolvimentoWeb/teste.js
@@ -0,0 +1,25 @@
+<li>
+                Ivysaur
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"
+                    alt="Ivysaur">
+            </li>
+            <li>
+                Venesaur
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+                    alt="Venesaur">
+            </li>
+            <li>
+                Charmander
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
+                    alt="Charmander">
+            </li>
+            <li>
+                Charmeleon
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg"
+                    alt="Charmeleon">
+            </li>
+            <li>
+                Charlizard
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"
+                    alt="Charlizard">
+            </li>
