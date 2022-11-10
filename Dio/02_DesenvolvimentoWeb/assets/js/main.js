f00a4b3c1244a1a8798a37b14a3f668c345e3eb6
ashow83 <adsf83@gmail.com>
2022-11-09 22:26:06 -0300

    manipulando API pokeAPI
    bootcamp DIO javascript

--- Dio/02_DesenvolvimentoWeb/assets/js/main.js
+++ Dio/02_DesenvolvimentoWeb/assets/js/main.js
@@ -1,51 +1,36 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemonli">
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             </div>
         </li >
 
     
-    `  
-              
+    `
+
 }
 
 const pokemonOl = document.getElementById('pokemonlist')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            //console.log(convertPokemonToLi(pokemon))
-            pokemonOl.innerHTML += convertPokemonToLi(pokemon)
-
-           
-        }
-
-
-    })
-    .catch((error) => console.error(error))
-    .finally(() => console.log('Requisição concluída'))
-
+pokeApi.getPokemons().then((pokemonList = []) => {
+    const newHtml = pokemonList.map(convertPokemonToLi).join('')
+    pokemonOl.innerHTML = newHtml
+})
 
 
 
